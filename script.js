a5af9d9a4a50f1a74ad91e0ebcaba8ccacb0a1cb
SHumona-Akter <shumona.cse.bd@gmail.com>
2023-01-29 20:30:33 +0600

    Show this as output 39 times.

--- script.js
+++ script.js
@@ -114,3 +114,9 @@ while(i <= 98){
     console.log(i);
     i++;
 }
+// আসকে আমার মন ভালো নেই এই কথা ৩৯ বার আউটপুট হিসেবে দেখাও। 
+
+for (var i = 0; i <= 39 ; i++){
+    console.log("aj k amar mon valo nei");
+    console.log(i)
+}
